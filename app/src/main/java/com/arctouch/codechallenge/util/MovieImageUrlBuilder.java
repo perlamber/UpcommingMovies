3c640494689c31f879a05e597bd4ef8814fde3db
Cristiano Ferreira <cristiano.oliveira2@fatec.sp.gov.br>
2018-07-08 22:26:08 -0300

    configuration references

--- app/src/main/java/com/arctouch/codechallenge/util/MovieImageUrlBuilder.java
+++ app/src/main/java/com/arctouch/codechallenge/util/MovieImageUrlBuilder.java
@@ -1,5 +1,6 @@
 package com.arctouch.codechallenge.util;
 
+import com.arctouch.codechallenge.api.ApiConfig;
 import com.arctouch.codechallenge.api.GenreService;
 
 public class MovieImageUrlBuilder {
@@ -8,10 +9,10 @@ public class MovieImageUrlBuilder {
     private static final String POSTER_URL = "https://image.tmdb.org/t/p/w154";
 
     public String buildPosterUrl(String posterPath) {
-        return POSTER_URL + posterPath + "?api_key=" + GenreService.API_KEY;
+        return POSTER_URL + posterPath + "?api_key=" + ApiConfig.API_KEY;
     }
 
     public String buildBackdropUrl(String backdropPath) {
-        return BACKDROP_URL + backdropPath + "?api_key=" + GenreService.API_KEY;
+        return BACKDROP_URL + backdropPath + "?api_key=" + ApiConfig.API_KEY;
     }
 }
